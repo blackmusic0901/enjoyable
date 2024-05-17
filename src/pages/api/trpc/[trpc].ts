e166c7e55d4b6d98cd5b161a6cad1e2a29e3e64a
yayadontknow <lwsong1234567@1utar.my>
2024-05-17 10:45:57 +0800

    temp: try extend timeout time

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -4,6 +4,13 @@ import { env } from "~/env.mjs";
 import { createTRPCContext } from "~/server/api/trpc";
 import { appRouter } from "~/server/api/root";
 
+
+// Configuration object with maxDuration set to 5 seconds
+export const config = {
+  maxDuration: 20,
+};
+
+
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
